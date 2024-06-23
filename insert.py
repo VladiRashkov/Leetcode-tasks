ebc9040b3bfb1faef54424321ef704125caf8508
Vladislav <vladislav.rashkoff@gmail.com>
2024-06-23 12:49:37 +0300

    Search Insert Position

---
+++ insert.py
@@ -0,0 +1,47 @@
+def binary_search(arr:list, target:int):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            print(f"Element is present at index {mid}.")
+            return mid 
+        elif arr[mid] < target:
+            left = mid + 1 
+        else:
+            right = mid - 1 
+    print(f"Target should be at index {left}.")
+    return left  
+
+
+
+result = binary_search(list(map(int, input().split(','))), int(input()))
+
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums contains distinct values sorted in ascending order.
+# -104 <= target <= 104
